e076714e33d73e4913a1583508ca0bd7c3256909
jaquepaschoal <jaque-paschoal@hotmail.com>
2018-03-12 13:57:19 -0300

    Modulo 2 - Até aula 12

---
+++ M2/src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+import Title from './app.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(
+  <Title/>,
+  document.querySelector('[data-js="app"]')
+)
